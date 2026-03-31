ebe1ec2f8f4270fa0ca280edf26662c85f441f35
Denis <oanadenis9@gmail.com>
2026-03-31 11:31:12 +0300

    nu mai suport

--- consultatie.h
+++ consultatie.h
@@ -12,9 +12,9 @@ class consultatie  {
         consultatie(std::string data, std::string ora, std::string nume_medic, std::string nume_pacient);
         /*
         std::string get_data();
-        std::string get_ora();
+        std::string get_ora();]
+        */
         std::string get_nume_medic();
         std::string get_nume_pacient();
-        */
 };
         
